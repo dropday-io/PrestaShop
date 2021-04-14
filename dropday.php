237484a5d46c495e7439b7b0a88003efe9fb358b
Matthijs <github@muyncky.eu>
2021-04-14 21:02:45 +0200

    style (module validation PrestaShop): change code style for validation and readability
    - following PSR standard
    - write more readable code (not too much things on one line, ternaries, readable variables names)
    - indenting

--- dropday.php
+++ dropday.php
@@ -76,12 +76,9 @@ class Dropday extends Module
         return $output;
     }
     
-    public function getApiUrl($type = '') {
-        if ($type) {
-            return trim($this->api_uri, '/') . '/' . $type;
-        } else {
-            return trim($this->api_uri, '/');
-        }
+    public function getApiUrl($type = '')
+    {
+        return $type ? trim($this->api_uri, '/') . '/' . $type : trim($this->api_uri, '/');
     }
 
     protected function renderForm()
@@ -186,15 +183,15 @@ class Dropday extends Module
             return false;
         }
         
-        $order = new Order((int)$id_order);
+        $order = new Order((int) $id_order);
         $old_os = $order->getCurrentOrderState();
         if (!Validate::isLoadedObject($order)) {
             return false;
         }
         
-        $shop = new Shop((int)$order->id_shop);
-        $customer = new Customer((int)$order->id_customer);
-        $address = new Address((int)$order->id_address_delivery);
+        $shop = new Shop((int) $order->id_shop);
+        $customer = new Customer((int) $order->id_customer);
+        $address = new Address((int) $order->id_address_delivery);
         $order_data = array(
             'external_id' => (int) $id_order,
             'source' => $shop->name,
@@ -209,7 +206,7 @@ class Dropday extends Module
                 'address2' => ($address->address2 ? $address->address2 : $address->address2),
                 'postcode' => $address->postcode,
                 'city' => $address->city,
-                'country' => Country::getNameById($order->id_lang, (int)$address->id_country),
+                'country' => Country::getNameById($order->id_lang, (int) $address->id_country),
                 'phone' => $address->phone,
             ),
             'products' => array()
@@ -219,24 +216,29 @@ class Dropday extends Module
         }
         $products = $order->getProducts();
         foreach ($products as $product) {
-            $cat = new Category((int) $product['id_category_default'], (int)$order->id_lang);
-            $p = array(
+            $cat = new Category((int) $product['id_category_default'], (int) $order->id_lang);
+            $quantity = (int) (isset($product['customizationQuantityTotal']) && $product['customizationQuantityTotal'])
+                ? $product['customizationQuantityTotal']
+                : $product['product_quantity'];
+            $link_rewrite = $this->getProductLinkRewrite((int) $product['product_id'], (int) $order->id_lang);
+            $image_url = $this->context->link->getImageLink($link_rewrite, $product['image']->id, ImageType::getFormattedName('large'));
+            $product_data = array(
                 'external_id' => (int) $product['product_id'],
                 'name' => ''.$product['product_name'],
                 'reference' => ''.$product['reference'],
-                'quantity' => (int) (isset($product['customizationQuantityTotal']) && $product['customizationQuantityTotal']) ? $product['customizationQuantityTotal'] : $product['product_quantity'],
+                'quantity' => $quantity,
                 'price' => (float) $product['product_price'],
-                'image_url' => $this->context->link->getImageLink($this->getProductLinkRewrite((int)$product['product_id'], (int)$order->id_lang), $product['image']->id, $this->image_format ),
+                'image_url' => $image_url,
                 'brand' => ''.Manufacturer::getNameById((int) $product['id_manufacturer']),
                 'category' => ''.$cat->name,
                 'supplier' => ''.Supplier::getNameById((int) $product['id_supplier']),
             );
             
             if (Tools::strlen($product['ean13']) >= 13) {
-                $p['ean13'] = $product['ean13'];
+                $product_data['ean13'] = $product['ean13'];
             }
             
-            $order_data['products'][] = $p;
+            $order_data['products'][] = $product_data;
         }
         
         $ch = curl_init();
@@ -261,14 +263,14 @@ class Dropday extends Module
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         $result = curl_exec($ch);
         if (curl_errno($ch)) {
-            Logger::addLog('[dropday] error: ' . curl_error($ch), 3, null, 'Order', (int)$id_order, true);
+            Logger::addLog('[dropday] error: ' . curl_error($ch), 3, null, 'Order', (int) $id_order, true);
         } else {
             $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
             $result = json_decode($result, true);
             if ($httpcode == 200) {
-                Logger::addLog('[dropday] Order created :#'.$result['order_id'], 1, null, 'Order', (int)$id_order, true);
+                Logger::addLog('[dropday] Order created :#'.$result['order_id'], 1, null, 'Order', (int) $id_order, true);
             } elseif ($httpcode == 422) {
-                Logger::addLog('[dropday] Error: ' . json_encode($result['errors']), 3, null, 'Order', (int)$id_order, true);
+                Logger::addLog('[dropday] Error: ' . json_encode($result['errors']), 3, null, 'Order', (int) $id_order, true);
             } else {
                 Logger::addLog('[dropday] Unknown error: ' . json_encode($result), 3, $httpcode, 'Order', $id_order, true);
             }
@@ -292,11 +294,11 @@ class Dropday extends Module
 
     public function hookActionOrderStatusUpdate($params)
     {
-        return $this->handleOrder((int)$params['id_order'], $params['newOrderStatus']);
+        return $this->handleOrder((int) $params['id_order'], $params['newOrderStatus']);
     }
 
     public function hookActionValidateOrder($params)
     {
-       return $this->handleOrder((int)$params['order']->id, $params['orderStatus']);
+        return $this->handleOrder((int) $params['order']->id, $params['orderStatus']);
     }
 }
